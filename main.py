67f82b306b00de2dd7c0d8d8c22dee72de89528b
Thinh Huynh Nguyen Truong <thinh.huynh@kyanon.digital>
2022-11-28 00:06:05 +0700

    MAE model

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from models_mae import mae_vit_large_patch16
+import torch
 
 model = mae_vit_large_patch16()
+
+checkpoint = torch.load('pretrained_weights/mae_pretrain_vit_large.pth', map_location='cpu')
+model.load_state_dict(checkpoint['model'], strict=False)
