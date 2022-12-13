37befe054ec452213d6f16365b1f040060ad923a
Thinh Huynh Nguyen Truong <thinh.huynh@kyanon.digital>
2022-12-13 18:16:10 +0700

    Update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from typing import OrderedDict
+from typing import List, OrderedDict
 from models_mae import mae_vit_large_patch16, mae_vit_huge_patch14
 from pprint import pprint
 import torch
+import json
 from torch import Tensor
 
 MODEL_TYPE = 'huge'
@@ -18,15 +19,33 @@ keys = list(model.state_dict().keys())
 
 state_dict: OrderedDict[str, Tensor] = model.state_dict()
 selected_state_dict = OrderedDict()
+selected_keys = []
 for key, value in state_dict.items():
     if key.startswith('blocks.'):
-        selected_state_dict[key.replace('blocks.', 'encoder.')] = value
+        key = key.replace('blocks.', 'encoder.')
+        selected_state_dict[key] = value
+        selected_keys.append(key)
     if key == 'cls_token':
         selected_state_dict['global_tokens'] = value
+        selected_keys.append('global_tokens')
     if key.startswith('patch_embed.'):
-        selected_state_dict[key.replace('patch_embed.', 'input_adapters.rgb.')] = value
+        key = key.replace('patch_embed.', 'input_adapters.rgb.')
+        selected_state_dict[key] = value
+        selected_keys.append(key)
         
-torch.save(OrderedDict(model=selected_state_dict), f'pretrained_weights/selected_mae_pretrain_vit_{MODEL_TYPE}.pth')
+# torch.save(OrderedDict(model=selected_state_dict), f'pretrained_weights/selected_mae_pretrain_vit_{MODEL_TYPE}.pth')
+
+def save_key(keys: List[str], file_path: str) -> None:
+    json_object = json.dumps(
+        dict(keys=keys), 
+        indent=4, 
+        ensure_ascii=False
+    ).encode('utf8')
+
+    with open(file_path, "wb") as f:
+        f.write(json_object)
+        
+save_key(selected_keys, f'mae_{MODEL_TYPE}_key.json')
 
 with open('key.txt', 'w') as f:
     f.write('\n'.join(keys))
