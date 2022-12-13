a3762237bea243734c689d07bbd5efe17eb96283
Thinh Huynh Nguyen Truong <thinh.huynh@kyanon.digital>
2022-12-13 10:25:26 +0700

    Update

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from typing import Any, Dict, List, Tuple, OrderedDict
-from models_mae import mae_vit_large_patch16
+from typing import OrderedDict
+from models_mae import mae_vit_large_patch16, mae_vit_huge_patch14
 from pprint import pprint
 import torch
 from torch import Tensor
 
-model = mae_vit_large_patch16()
+MODEL_TYPE = 'huge'
 
-checkpoint = torch.load('pretrained_weights/mae_pretrain_vit_large.pth', map_location='cpu')
+if MODEL_TYPE == 'large':
+    model = mae_vit_large_patch16()
+elif MODEL_TYPE == 'huge':
+    model = mae_vit_huge_patch14()
+
+checkpoint = torch.load(f'pretrained_weights/mae_pretrain_vit_{MODEL_TYPE}.pth', map_location='cpu')
 model.load_state_dict(checkpoint['model'], strict=False)
 
 keys = list(model.state_dict().keys())
@@ -21,7 +26,7 @@ for key, value in state_dict.items():
     if key.startswith('patch_embed.'):
         selected_state_dict[key.replace('patch_embed.', 'input_adapters.rgb.')] = value
         
-torch.save(OrderedDict(model=selected_state_dict), 'pretrained_weights/selected_mae_pretrain_vit_large.pth')
+torch.save(OrderedDict(model=selected_state_dict), f'pretrained_weights/selected_mae_pretrain_vit_{MODEL_TYPE}.pth')
 
 with open('key.txt', 'w') as f:
     f.write('\n'.join(keys))
