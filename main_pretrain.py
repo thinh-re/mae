3f14e4faf494b4fd47b2777ed6295787df6596a1
Thinh Huynh Nguyen Truong <thinh.huynh@kyanon.digital>
2022-12-20 00:00:34 +0700

    Update

--- main_pretrain.py
+++ main_pretrain.py
@@ -74,7 +74,7 @@ def get_args_parser():
                         help='epochs to warmup LR')
 
     # Dataset parameters
-    parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
+    parser.add_argument('--data_path', default='/kaggle/input/imagenet-object-localization-challenge/ILSVRC/Data/CLS-LOC/', type=str,
                         help='dataset path')
 
     parser.add_argument('--output_dir', default='./output_dir',
---
+++ setup_python3_10.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes ppa:deadsnakes/ppa
+sudo apt install -y python3.10 python3.10-dev python3.10-venv
+python3.10 --version
+python3.10 -m venv env
+./env/bin/pip install --upgrade pip setuptools wheel
+./env/bin/pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+./env/bin/pip install -r requirements/train.txt
